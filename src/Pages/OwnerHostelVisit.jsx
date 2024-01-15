a20aced7eeec940be8dc0c30ce95400817fdc2a7
AMALRAJ PM <121855823+kayilwalker@users.noreply.github.com>
2024-01-15 12:15:13 +0530

    third commit

--- src/Pages/OwnerHostelVisit.jsx
+++ src/Pages/OwnerHostelVisit.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react'
-import OwnerAddedCardsVisit from '../Components/OwnerAddedCardsVisit'
 import { deleteHostelAPI, ownerHostelAPI } from '../services/allAPI'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 import { addHostelResponseContext, editHostelResoponseContext } from '../context/ContextShare'
